40c69948bab471eaab8d06577e05833930aecbfb
juliocbms <juliocesar13b@gmail.com>
2025-03-27 19:30:12 -0300

    commit

---
+++ src/app/clients/components/perfil-form/perfil-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil-form',
+  imports: [],
+  templateUrl: './perfil-form.component.html',
+  styleUrl: './perfil-form.component.scss'
+})
+export class PerfilFormComponent {
+
+}
